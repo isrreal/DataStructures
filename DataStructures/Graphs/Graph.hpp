868c986228d701cbede3c517c22f1e0407a6f02a
leggen-assis <souzaferreira437@gmail.com>
2024-10-01 15:51:16 -0300

    Graph.hpp updated

--- DataStructures/Graphs/Graph.hpp
+++ DataStructures/Graphs/Graph.hpp
@@ -4,38 +4,52 @@
 #include "Vertex.hpp"
 #include <queue>
 #include <stack>
-
+#include <random>
+#include <algorithm>
+#include <vector>
+#include <list>
 
 class Graph {
 private:
-    int vertexesAmount;
-    int edgesAmount;
+    int order;
+    int size;
     bool isDirected;
-    std::list<Vertex*> vertexes;
-    std::vector<std::pair<int, int>> edges;
-    void configureEdges(std::vector<std::pair<int, int>> edges);
-public:
-    Graph(std::list<Vertex*> vertex,
-        std::vector<std::pair<int, int>> edges, bool isDirected);
+
+    std::vector<Vertex*> vertices;
+    std::vector<std::list<int>> edges;
+
+    void addEdge(int source, int destination);
+    void sortEdges();
+    void removeRedundantEdges();
+   // void configureEdges(std::vector<std::pair<int, int>> edges);
+
+public:	
+
+    Graph(size_t order, bool isDirected);	
     Graph(const Graph& graph);
-
     Graph();
-
     ~Graph();
+ 
+    int getSize();
+    int getOrder();
+    int getVertexDegree(Vertex* vertex);
+    std::vector<Vertex*> getVertices();
+    void getAdjacencyList(Vertex* vertex);	
+
+    void breadthFirstSearch();  
+
+    void depthFirstSearch();
     
-    int getAmountOfEdges() { return this->edgesAmount; }
-
-    int getAmountOfVertexes() { return this->vertexesAmount; }
-
-    std::list<Vertex*> getVertexes() { return this->vertexes; }
-
-    void breadthFirstSearch(Vertex* start, Vertex* destiny);  
-
-    void depthFirstSearch(Vertex* start);
+    void deleteAdjacencyList(Vertex* vertex);
+    
 
     friend std::ostream& operator<< (std::ostream& os, const Graph& graph) {
-        for (const auto& it: graph.vertexes)
-            os << *it << std::endl;
+        for (size_t i = 0; i < graph.edges.size(); ++i) {
+		std::cout << graph.vertices[i]->identificator << " ---> ";
+		for (const auto& it: graph.edges[i])
+			std::cout << it << " ";
+		std::cout << std::endl;
+	}
         return os;
     }
 };
