90e404bc6199012a958d73857c462d3244b604c0
leggen-assis <souzaferreira437@gmail.com>
2024-10-26 09:47:20 -0300

    graph implementation was changed

--- DataStructures/Graphs/Graph.cpp
+++ DataStructures/Graphs/Graph.cpp
@@ -1,97 +1,129 @@
 #include "Graph.hpp"
 
-void Graph::removeRedundantEdges() {
-	auto newVector = std::unique(this->edges.begin(), this->edges.end());
-	this->edges.erase(newVector, this->edges.end());
-}
-
-Graph::Graph(size_t order, bool isDirected) {
+Graph::Graph(size_t order, bool isDirected, float probabilityOfEdge) {
     this->isDirected = isDirected;
+    this->order = order;
+    this->size = 0;
+
+    size_t connectedVertex = 0;
+    float probability = 0.0;
+
     std::random_device randomNumber;
     std::mt19937 seed(randomNumber());
-    std::uniform_int_distribution<int> gap(0, order - 1); 
+    std::uniform_int_distribution<int> gap(0, order - 1);
+    std::uniform_real_distribution<float> probabilityGap(0.0, 1.0);
 
-    this->vertices = std::vector<Vertex*>(order);
-    this->edges = std::vector<std::list<int>>(order);
-    this->size = 0;
-    
+    for (size_t i = 0; i < order; ++i)
+        adjList[i] = {};
 
-    for (size_t i = 0; i < order; ++i) 
-        this->vertices[i] = new Vertex(i, gap(seed) % 4);
-   
-    for (size_t i = 1; i < order; ++i) 
-    	this->addEdge(i - 1, i); 
+    for (size_t i = 0; i < order; ++i) {
+        connectedVertex = gap(seed);
 
-    for (size_t i = 0; i < order * 2; ++i) { 
-        int source = gap(seed);
-        int destination = gap(seed);
+        while (i == connectedVertex)
+            connectedVertex = gap(seed);
 
+        if (!edgeExists(i, connectedVertex))
+            addEdge(i, connectedVertex);
 
-        if (source != destination) {
-            auto& adjList = edges[source];
-            if (std::find(adjList.begin(), adjList.end(), destination) == adjList.end()) {
-                this->addEdge(vertices[source]->identificator, vertices[destination]->identificator);
-            }
+        for (size_t j = i + 1; j < order; ++j) {
+            if (!edgeExists(i, j)) {
+                probability = probabilityGap(seed);
+                if (probabilityOfEdge >= probability)
+                    addEdge(i, j);
+           }
         }
     }
-
 }
 
+Graph::Graph(const std::string& filename, bool isDirected) {
+	this->isDirected = isDirected;
+   	std::ifstream file(filename, std::fstream::in);
+    
+    if (!file) {
+        std::cerr << "Error opening the file!" << std::endl;
+        throw std::runtime_error("File not found");
+    }
+
+    std::string line;
+    size_t source = 0, destination = 0;
+
+    // Lê a primeira linha para obter order e size
+    if (std::getline(file, line)) {
+        std::stringstream ss(line);
+        ss >> this->order >> this->size; // Captura o número de vértices e arestas
+    }
+
+    // Inicializa a lista de adjacência para cada vértice
+    for (size_t i = 0; i < order; ++i) {
+        adjList[i] = {};
+    }
+
+    // Lê as arestas restantes
+    while (std::getline(file, line) && (adjList.size() < size)) {
+        std::stringstream ssEdges(line);
+        
+        while (ssEdges >> source >> destination) {
+            addEdge(source, destination);
+            // Verifica se o número de arestas já atingiu o tamanho desejado
+            if (adjList.size() >= size) break;
+        }
+    }
+    
+    // addEdge increments size a value a twice 
+    this->size = size / 2;
+}
 
 Graph::Graph(const Graph& graph) {
-    this->vertices = graph.vertices;
-    this->edges = graph.edges;
-    this->order = graph.order; 
+    this->adjList = graph.adjList;
+    this->order = graph.order;
     this->size = graph.size;
     this->isDirected = graph.isDirected;
 }
 
 Graph::Graph() {}
 
-// Destructor
-
-Graph::~Graph() {
-    for (auto& it: vertices)
-        delete it;
-}
-
-void Graph::addEdge(int source, int destination) {
+void Graph::addEdge(size_t source, size_t destination) {
     if (this->isDirected == false) {
-        this->edges[source].push_back(destination);
-        this->edges[destination].push_back(source);
-        this->size += 2;  
+        this->adjList[source].push_back(destination);
+        this->adjList[destination].push_back(source);
+        this->size += 2;
     } else {
-        this->edges[source].push_back(destination);
+        this->adjList[source].push_back(destination);
         this->size += 1;
     }
 }
 
+bool Graph::edgeExists(size_t u, size_t v) {
+    return std::find(adjList[u].begin(), adjList[u].end(), v) != adjList[u].end();
+}
 
-int Graph::getVertexDegree(Vertex* vertex) { return this->edges[vertex->identificator].size(); }
+size_t Graph::getVertexDegree(size_t vertex) { return this->adjList[vertex].size(); }
 
-int Graph::getSize() { return this->size; }
+size_t Graph::getSize() { return this->size; }
 
-int Graph::getOrder() { return this->order; }
+size_t Graph::getOrder() { return this->order; }
 
-std::vector<Vertex*> Graph::getVertices() { return this->vertices; }
-	
+std::unordered_map<size_t, std::list<size_t>> Graph::getAdjacencyList() { return this->adjList; }
 
-// public methods
+std::list<size_t> Graph::getAdjacencyList(size_t vertex) { return this->adjList[vertex]; }
 
+bool Graph::vertexExists(size_t vertex) { return adjList.find(vertex) != adjList.end(); }
+
+/*
 void Graph::breadthFirstSearch() {
     std::vector<bool> visited(this->order, false);
     std::queue<int> fila;
-    visited[vertices[0]->identificator] = true;  // Marcar o vértice inicial como visitado
+    visited[vertices[0]->identificator] = true;
     fila.push(vertices[0]->identificator);
-    
+
     while (!fila.empty()) {
         int temp = fila.front();
         fila.pop();
-        std::cout << temp << std::endl;  // Imprimir o vértice visitado
+        std::cout << temp << std::endl;
 
         for (const auto& it : edges[temp]) {
             if (!visited[it]) {
-                visited[it] = true;  // Marcar como visitado antes de adicionar à fila
+                visited[it] = true;
                 fila.push(it);
             }
         }
@@ -101,33 +133,61 @@ void Graph::breadthFirstSearch() {
 void Graph::depthFirstSearch() {
     std::vector<bool> visited(this->order, false);
     std::stack<int> stack;
-    stack.push(vertices[0]->identificator); 
+    stack.push(vertices[0]->identificator);
 
     while (!stack.empty()) {
         int temp = stack.top();
         stack.pop();
 
         if (!visited[temp]) {
-            visited[temp] = true;  // Marcar como visitado
-            std::cout << temp << std::endl;  // Imprimir o vértice visitado
+            visited[temp] = true;
+            std::cout << temp << std::endl;
 
             for (const auto& neighbor : edges[temp]) {
-                if (!visited[neighbor])  
-                    stack.push(neighbor);          
+                if (!visited[neighbor])
+                    stack.push(neighbor);
             }
         }
     }
 }
 
-void Graph::getAdjacencyList(Vertex* vertex) {
-	if (!edges[vertex->identificator].empty())
-		for (auto it: edges[vertex->identificator])
-			std::cout << it << " ";
-	else
-		std::cout << "There is no adjacencies!" << std::endl;
+*/
+
+void Graph::deleteAdjacencyList(size_t vertex) {
+    if (adjList.find(vertex) == adjList.end())
+    	return;
+
+    std::queue<size_t> toDelete;
+    toDelete.push(vertex);
+    int currentVertex = -1;
+
+    for (const auto& it: this->adjList[vertex])
+    	toDelete.push(it);
+
+    while (!toDelete.empty()) {
+        currentVertex = toDelete.front();
+        toDelete.pop();
+
+        for (const auto& it: this->adjList[currentVertex])
+        	this->adjList[it].remove(currentVertex);
+       	deleteVertex(currentVertex);
+    }
 }
 
+void Graph::deleteVertex(size_t vertex) {
+    this->size -= this->adjList[vertex].size();
+    this->adjList.erase(vertex);
+    --this->order;
+}
 
-void Graph::deleteAdjacencyList(Vertex* vertex) {
-	this->edges[vertex->identificator].clear();
+std::ostream& operator<< (std::ostream& os, const Graph& graph) {
+    for (const auto& pair : graph.adjList) {
+        size_t vertex = pair.first;  
+
+        os << vertex << " ----> ";
+        for (const auto& neighbor : pair.second) 
+            os << neighbor << " ";  
+        os << std::endl;
+    }
+    return os;
 }
--- DataStructures/Graphs/Graph.hpp
+++ DataStructures/Graphs/Graph.hpp
@@ -1,57 +1,57 @@
 #ifndef GRAPH_HPP
 #define GRAPH_HPP
 
-#include "Vertex.hpp"
-#include <queue>
-#include <stack>
+#include <iostream>
 #include <random>
 #include <algorithm>
-#include <vector>
 #include <list>
+#include <unordered_map>
+#include <queue>
+#include <sstream>
+#include <fstream>
 
 class Graph {
 private:
-    int order;
-    int size;
-    bool isDirected;
-
-    std::vector<Vertex*> vertices;
-    std::vector<std::list<int>> edges;
-
-    void addEdge(int source, int destination);
-    void sortEdges();
-    void removeRedundantEdges();
-   // void configureEdges(std::vector<std::pair<int, int>> edges);
-
+    size_t order;
+    size_t size;
+    bool isDirected;  
+    std::unordered_map<size_t, std::list<size_t>> adjList;
+    void addEdge(size_t source, size_t destination);
+    Graph readGraph(const std::string& filename);
 public:	
 
+    Graph(size_t order, bool isDirected, float probabilityOfEdge);	
+    Graph(const std::string& filename, bool isDirected);
     Graph(size_t order, bool isDirected);	
     Graph(const Graph& graph);
     Graph();
-    ~Graph();
+    ~Graph() = default;
  
-    int getSize();
-    int getOrder();
-    int getVertexDegree(Vertex* vertex);
-    std::vector<Vertex*> getVertices();
-    void getAdjacencyList(Vertex* vertex);	
+    size_t getSize();
+    size_t getOrder();
+    size_t getVertexDegree(size_t vertex);
+    
+    std::unordered_map<size_t, std::list<size_t>> getAdjacencyList();
+    
+    std::list<size_t> getAdjacencyList(size_t vertex);
+    
+    bool edgeExists(size_t u, size_t v);
+    
+    bool vertexExists(size_t vertex);	
 
     void breadthFirstSearch();  
-
+    
+    void setVertexLabel(size_t vertex, int label);
+    
+    void setAdjacenciesLabel(size_t vertex, int label);
+    
     void depthFirstSearch();
     
-    void deleteAdjacencyList(Vertex* vertex);
+    void deleteAdjacencyList(size_t vertex);
     
-
-    friend std::ostream& operator<< (std::ostream& os, const Graph& graph) {
-        for (size_t i = 0; i < graph.edges.size(); ++i) {
-		std::cout << graph.vertices[i]->identificator << " ---> ";
-		for (const auto& it: graph.edges[i])
-			std::cout << it << " ";
-		std::cout << std::endl;
-	}
-        return os;
-    }
+    void deleteVertex(size_t vertex);
+    
+    friend std::ostream& operator<< (std::ostream& os, const Graph& graph);
 };
 
 #endif
